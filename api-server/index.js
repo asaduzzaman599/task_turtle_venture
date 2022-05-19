37c98d7905ef05d14bec7599214ee9421a3d9a2b
asaduzzaman599 <asaduzzamansoumit@gmail.com>
2022-05-19 19:13:10 +0600

    firabase service added

--- api-server/index.js
+++ api-server/index.js
@@ -6,7 +6,28 @@ const app = express()
 const port = process.env.PORT || 5000
 
 
+app.use(cors())
+app.use(express.json())
 
+//firebase endpoint authentications
+const admin = require("firebase-admin");
+
+var serviceAccount = require("./service.json");
+
+admin.initializeApp({
+    credential: admin.credential.cert(serviceAccount)
+});
+
+app.post('/signup', async (req, res) => {
+    const { email, password } = req.body
+    if (email && password) {
+        const authResponse = await admin.auth().createUser({
+            email, password, emailVerified: false, disabled: false
+        })
+        res.send(authResponse)
+    }
+
+})
 app.get('/', (req, res) => {
     res.send(`server running port at : ${port} `)
 })
